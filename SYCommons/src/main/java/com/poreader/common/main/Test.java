265454c2d6fff5ba2fefe8fde9aac39fc1c68328
xsi640 <xsi640@hotmail.com>
2015-04-13 16:11:59 +0800

    import cache.

--- SYCommons/src/main/java/com/poreader/common/main/Test.java
+++ SYCommons/src/main/java/com/poreader/common/main/Test.java
@@ -1,6 +1,19 @@
 package com.poreader.common.main;
 
+import com.poreader.common.cache.StandardHotCache;
+
 public class Test {
 	public static void main(String[] args) {
+		StandardHotCache<String, String> cache = new StandardHotCache<String, String>();
+		cache.setDetectSpanInSecs(1);
+		cache.setMaxMuteSpanInSecs(10);
+		cache.initialize();
+		cache.add("1", "suyang");
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println(cache.get("1"));
 	}
 }
